a2185eb84dcefcd70520e9fa4f16017c42950edb
shsaaad <shmahee118@gmail.com>
2023-02-11 16:23:16 +0600

    color update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let changePurple = document.getElementById('purple');
 changePurple.onclick = function changePurple() {
-    document.body.style.backgroundColor = '#CE40F5';
+    document.body.style.backgroundColor = '#9b59b6';
 }
 let changeBlue = document.getElementById('blue');
 changeBlue.onclick = function changeBlue() {
-    document.body.style.backgroundColor = '#1972DE';
+    document.body.style.backgroundColor = '#3498db';
 }
 let changeOrange = document.getElementById('orange');
 changeOrange.onclick = function changeOrange() {
-    document.body.style.backgroundColor = 'orange';
+    document.body.style.backgroundColor = '#e67e22';
 }
 let reset = document.getElementById('reset');
 reset.onclick = function reset() {
