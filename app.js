f1d8f1a9647bb7ac77c5b7eec63b0c7bbf329fbe
shsaaad <shmahee118@gmail.com>
2023-02-11 15:15:04 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let changePurple = document.getElementById('purple');
+changePurple.onclick = function changePurple() {
+    document.body.style.backgroundColor = '#CE40F5';
+}
+let changeBlue = document.getElementById('blue');
+changeBlue.onclick = function changeBlue() {
+    document.body.style.backgroundColor = '#1972DE';
+}
+let changeOrange = document.getElementById('orange');
+changeOrange.onclick = function changeOrange() {
+    document.body.style.backgroundColor = 'orange';
+}
+let reset = document.getElementById('reset');
+reset.onclick = function reset() {
+    document.body.style.backgroundColor = '#fff';
+}
